5b257367195fc892f71d2fda505bd8d80a905726
galihatha <49637517+galihatha@users.noreply.github.com>
2023-06-12 16:20:39 +0700

    Update visualize_data.py

--- visualize_data.py
+++ visualize_data.py
@@ -1,20 +1,24 @@
-import streamlit as st
 import pandas as pd
+import streamlit as st
 from bokeh.plotting import figure
 from bokeh.models import HoverTool, ColumnDataSource
 from bokeh.models import CategoricalColorMapper
 from bokeh.palettes import Spectral6
 
-# Load the data
+# Read the data
 data = pd.read_csv("https://raw.githubusercontent.com/galihatha/Visdat/main/data-penumpang-bus-transjakarta-januari-desember-2021.csv")
 data.set_index('bulan', inplace=True)
+
+# Remove rows with NaN values in 'jenis' column
 data = data.dropna(subset=['jenis'])
+
+# Convert 'jenis' column to string
 data['jenis'] = data['jenis'].astype(str)
 
-# Make a color mapper
+# Make a color mapper: color_mapper
 color_mapper = CategoricalColorMapper(factors=data['jenis'].unique().tolist(), palette=Spectral6)
 
-# Create the figure
+# Create the figure: plot
 plot = figure(title='Data Penumpang Bus TransJakarta', x_axis_label='Jenis', y_axis_label='Kode Trayek',
               plot_height=400, plot_width=700, tools=[HoverTool(tooltips='@trayek')])
 
@@ -22,41 +26,33 @@ plot = figure(title='Data Penumpang Bus TransJakarta', x_axis_label='Jenis', y_a
 plot.circle(x='x', y='y', source=source, fill_alpha=0.8,
             color=dict(field='x', transform=color_mapper), legend='jenis')
 
-# Define the update_plot function
+# Set the legend and axis attributes
+plot.legend.location = 'bottom_left'
+
+# Define the callback function: update_plot
 def update_plot(month, x, y):
-    # Filter the data based on the selected month
-    filtered_data = data[data.index == month]
-    
-    # Create a new data source
-    source = ColumnDataSource(data={
-        'x': filtered_data[x],
-        'y': filtered_data[y],
-        'trayek': filtered_data['trayek'],
-        'jumlah_penumpang': filtered_data['jumlah_penumpang']
-    })
-    
-    # Update the plot with the new data
-    plot.title.text = 'Data Penumpang Bus TransJakarta - Bulan %s' % month
+    # Label axes of plot
     plot.xaxis.axis_label = x
     plot.yaxis.axis_label = y
-    plot.source = source
+    # new data
+    new_data = {
+        'x': data[data.index == month][x],
+        'y': data[data.index == month][y],
+        'trayek': data[data.index == month]['trayek'],
+        'jumlah_penumpang': data[data.index == month]['jumlah_penumpang']
+    }
+    source.data = new_data
+    
+    # Add title to figure: plot.title.text
+    plot.title.text = 'Data Penumpang Bus TransJakarta - Bulan %s' % month
 
-# Create the Streamlit app
-def main():
-    st.title('Data Penumpang Bus TransJakarta')
-    
-    # Create the slider for selecting the month
-    month = st.slider('Bulan', 1, 12, 1)
-    
-    # Create the dropdown menus for selecting x and y axis
-    x = st.selectbox('x-axis data', ['jenis', 'kode_trayek', 'trayek', 'jumlah_penumpang'])
-    y = st.selectbox('y-axis data', ['jenis', 'kode_trayek', 'trayek', 'jumlah_penumpang'])
-    
-    # Update the plot when the inputs are changed
-    update_plot(month, x, y)
-    
-    # Render the plot
-    st.bokeh_chart(plot)
+# Create layout
+st.title("Data Penumpang Bus TransJakarta")
+month = st.slider("Bulan", min_value=1, max_value=12, step=1, value=1)
+x_select = st.selectbox("x-axis data", options=['jenis', 'kode_trayek', 'trayek', 'jumlah_penumpang'], index=0)
+y_select = st.selectbox("y-axis data", options=['jenis', 'kode_trayek', 'trayek', 'jumlah_penumpang'], index=1)
 
-if __name__ == '__main__':
-    main()
+update_plot(month, x_select, y_select)
+
+# Add the plot to Streamlit app
+st.bokeh_chart(plot)
